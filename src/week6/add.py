34ca1c98b2c5bfa50a2c38a24afaa635d4cc766c
Hojun Kim <hojunkim02@icloud.com>
2026-06-29 16:49:56 +0900

    update

---
+++ src/week6/add.py
@@ -0,0 +1,9 @@
+x = input("첫 번째 정수: ")
+y = input("두 번째 정수: ")
+sum = x + y
+print("합은", sum)
+
+x = int(input("첫 번째 정수: "))
+y = int(input("두 번째 정수: "))
+sum = x + y
+print("합은", sum)
---
+++ src/week6/name_age.py
@@ -0,0 +1,5 @@
+name = input("이름이 무엇인가요? ")
+print("만나서 반갑습니다.", name + "씨!")
+
+age = input("나이는요? ")
+print("네, 당신은 " + age + "살이시군요 " + name + "씨!")
